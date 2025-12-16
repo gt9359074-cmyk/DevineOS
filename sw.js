b495fa735de060cff7335e4b9ccf97405f890ce9
DevineLauncher <gt9359074@gmail.com>
2025-12-15 21:50:07 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,16 @@
-/* --- DIVINE OS SERVICE WORKER --- */
-/* This script allows the OS to be installed and run offline */
-
-const CACHE_NAME = 'divine-os-v1';
-const ASSETS_TO_CACHE = [
-  './index.html',
-  './manifest.json',
-  './icon.png'
+const CACHE_NAME = 'divine-os-v2'; // Version bump
+const ASSETS = [
+    './index.html',
+    './index.html?mode=standalone', // The secret App URL
+    './manifest.json',
+    './icon.png'
 ];
 
-// 1. INSTALL: When you first visit, save the OS to the browser's memory
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting(); // Force the new version to activate immediately
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+    self.skipWaiting();
 });
 
-// 2. FETCH: When you open the app, try to load from memory first
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
